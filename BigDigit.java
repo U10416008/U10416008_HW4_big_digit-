124e34a57a5a9e4a0143899d8e85ef728d2ffa20
U10416008 <cp4n3gj9448@gmail.com>
2016-03-19 13:16:59 +0800

    Version Quotient2
    Modify  quotient method
    add remainder

--- BigDigit.java
+++ BigDigit.java
@@ -171,59 +171,64 @@ public class BigDigit {
         }
         quo++;
 
-        while(minus.size() > digit2.length() || (minus.size() == digit2.length() && Integer.parseInt(minus.get(minus.size()-compare)) >= Integer.parseInt(number2[number2.length -compare]))){
-        for(int i = 0 ; i < number2.length;i++){
+		while((minus.size() > digit2.length() || (minus.size() == digit2.length() && Integer.parseInt(minus.get(minus.size()-compare)) >= Integer.parseInt(number2[number2.length -compare]))) ){
+            for(int i = 0 ; i < number2.length;i++){
             
-            lastLocation = lastLocation(Integer.parseInt(number1[i]),Integer.parseInt(number2[i]));
-            if(Integer.parseInt(number1[number1.length-1]) <= 0){
-                lastLocation = 0;
-            }
-            number1[i] = Integer.toString(minusDigit(Integer.parseInt(number1[i]),Integer.parseInt(number2[i])));
-            if(lastLocation == 1 ){
-                if(Integer.parseInt(number1[i+1]) !=0){
-                    number1[i+1] = Integer.toString(Integer.parseInt(number1[i+1]) -lastLocation);
-                }else{
-                    while(Integer.parseInt(number1[i+j+1]) == 0){
-                        number1[i+j+1] = "9";
-                        j++;
-                    }
-                    number1[i+j+1] = Integer.toString(Integer.parseInt(number1[i+j+1]) -lastLocation);
+                lastLocation = lastLocation(Integer.parseInt(number1[i]),Integer.parseInt(number2[i]));
+                if(Integer.parseInt(number1[minus.size()-1]) <= 0){
+                    lastLocation = 0;
                 }
+                number1[i] = Integer.toString(minusDigit(Integer.parseInt(number1[i]),Integer.parseInt(number2[i])));
+                if(lastLocation == 1 ){
+                    if(Integer.parseInt(number1[i+1]) !=0){
+                        number1[i+1] = Integer.toString(Integer.parseInt(number1[i+1]) -lastLocation);
+                    }else{
+                        while(Integer.parseInt(number1[i+j+1]) == 0){
+                            number1[i+j+1] = "9";
+                            j++;
+                        }
+                        number1[i+j+1] = Integer.toString(Integer.parseInt(number1[i+j+1]) -lastLocation);
+                    }
                 
+                }
+                j = 0;
             }
-            j = 0;
-        }
-        if(number2.length != minus.size()){
-            for(int i = number2.length; i < minus.size() ; i++){
-                number1[i] = Integer.toString(minusDigit(Integer.parseInt(number1[i])));
-                lastLocation = lastLocation(Integer.parseInt(number1[i]),lastLocation);
+            if(number2.length != minus.size()){
+                for(int i = number2.length; i < minus.size() ; i++){
+                    number1[i] = Integer.toString(minusDigit(Integer.parseInt(number1[i])));
+                    lastLocation = lastLocation(Integer.parseInt(number1[i]),lastLocation);
+                }
             }
-        }
-        for(int i = 0 ; i < minus.size() ; i++){
-            minus.set(i,number1[i]);
-        }
-        checkFirst0 = minus.size()-1;
-        while(Integer.parseInt(minus.get(checkFirst0)) == 0 && checkFirst0 > 0){
-            minus.remove(checkFirst0);
-            checkFirst0--;
-        }
+            for(int i = 0 ; i < minus.size() ; i++){
+                minus.set(i,number1[i]);
+            }
+            checkFirst0 = minus.size()-1;
+            while(Integer.parseInt(minus.get(checkFirst0)) == 0 && checkFirst0 > 0){
+                minus.remove(checkFirst0);
+                checkFirst0--;
+            }
         
        
         
         
-        if(minus.size() == digit2.length()){
-            compare = 1;
-            while(Integer.parseInt(minus.get(minus.size()-compare)) == Integer.parseInt(number2[number2.length -compare]) && compare < minus.size()){
-                compare++;
+            if(minus.size() == digit2.length()){
+                compare = 1;
+                while(Integer.parseInt(minus.get(minus.size()-compare)) == Integer.parseInt(number2[number2.length -compare]) && compare < minus.size()){
+                    compare++;
+                }
             }
-        }
         
         quo++;
         }
-        System.out.print("The Quotient result is ");
-        System.out.print(quo);
+        System.out.print("The QUOTIENT result is " + quo + ", and REMAINDER is ");
+        for(int i = minus.size()-1 ; i >= 0 ; i--){
+            System.out.print(minus.get(i));
+        }
         System.out.println();
-		
+        
+        
+            
+        
     }
 	//check number1 + number2 + nextLocation is >=10 or not
     public static int nextLocation(int number1,int number2,int nextLocation){
